5d6a50a564ee0a729d178e129ac9cbe34d80c8cb
Bryan Hann <nobody@nowhere>
2021-08-06 17:56:46 -0230

    Directly clone [manjaro.vbox]

--- main.sh
+++ main.sh
@@ -4,10 +4,28 @@ mkdir -p  ${sandbox}
 cd        ${sandbox}
 
 
-src=https://raw.githubusercontent.com/bryanhann/manjaro.vbox/master/home/setup
-dst=${HOME}/tmp/setup.d
+#src=https://raw.githubusercontent.com/bryanhann/manjaro.vbox/master/home/setup
+##dst=${HOME}/tmp/setup.d
 
 wget      ${src}
-chmod +x ./setup
-./setup
+#chmod +x ./setup
+#./setup
+
+
+src=https://github.com/bryanhann/manjaro.vbox
+dst=${PWD}/manjaro.vbox
+
+echo '-->' setup src: ${src}
+echo '-->' setup dst: ${dst}
+echo '-->' cloning.
+
+[ -d ${dst} ] || git clone ${src} ${dst}
+
+echo '-->' $ cd ${dst}
+             cd ${dst}
+echo '-->' $ git pull
+             git pull
+echo '-->' $ ./main.sh
+             ./main.sh
+
 
