5d24166a247d409fa510a9f1cc327419a89e9b05
Bryan Hann <nobody@nowhere>
2021-08-06 18:25:43 -0230

    Streamline.

--- main.sh
+++ main.sh
@@ -2,16 +2,10 @@
 src=https://github.com/bryanhann/manjaro.vbox
 dst=/tmp/manjaro.vbox.$RANDOM
 
-echo '-->' setup src: ${src}
-echo '-->' setup dst: ${dst}
-echo '-->' cloning.
-
-[ -d ${dst} ] || git clone ${src} ${dst}
+git clone ${src} ${dst}
 
 echo '-->' $ cd ${dst}
              cd ${dst}
-echo '-->' $ git pull
-             git pull
 echo '-->' $ ./main.sh
              ./main.sh
 
